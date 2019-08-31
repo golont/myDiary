b23c17e0184e8bf40a0251b8fd3e9004bca65dbb
golont <vladpanda123@gmail.com>
2019-08-31 15:20:14 +0300

    not-found page

--- src/js/components/pages/not-found-page/not-found-page.js
+++ src/js/components/pages/not-found-page/not-found-page.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const NotFoundPage = () => {
-    return <div>PageNotFound</div>;
+    return (
+        <section className="section-not-found">
+            <img src="/assets/img/not-found.png" alt="Not found" />
+            <h2 className="title">This page isn't available.</h2>
+            <h3 className="title">Sorry about that.</h3>
+        </section>
+    );
 };
 
 export default NotFoundPage;
